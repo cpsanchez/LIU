5dce849f1605c78cfa084971fcc6a48dfaf40d05
Clara Puerto Sanchez <puertosanchezclara@gmail.com>
2022-12-04 10:51:45 +0100

    Results

--- MCMC.py
+++ MCMC.py
@@ -144,7 +144,7 @@ nx = 200 # number of pixels of your image in X
 ny = 200 # number of pixels of your image in Y
 
 rm = 15.1*pixel_scale*dstar/2
-itilt = 30. # inclination of your disk in degrees
+itilt = 80. # inclination of your disk in degrees
 a = 8*rm # semimajoraxis of the disk in au
 ksi0 = 0.03*a # rerence scale height at the semi-major axis of the disk*a
 gamma = 2. # exponant of the vertical exponential decay
